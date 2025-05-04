f2b41210a95f8988d39c1d5059ba62e8699e8a95
Amit <amitbtdev@gmail.com>
2025-05-04 20:02:27 +0300

    Add setup.sh and backend/requirements.txt for Render deployment

--- setup.sh
+++ setup.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
-# Fail on first error
-set -o errexit
-
-# Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
-
-# Install Node dependencies
-npm install
-
-# Build frontend
-npm run build
